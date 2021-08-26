5429808956d35a23cb814610d7bfc070d424ac99
chiragvariance <chirag@varianceinfotech.com>
2021-08-27 00:42:43 +0530

    Sprint 3 changes commited.

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,7 +5,9 @@ namespace LearnGit.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        /// <summary>
+        /// Comment added in Sprint3
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
